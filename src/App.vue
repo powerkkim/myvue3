795f43ce205a1c8c3682aee91d12ce7fb8525dc5
powerkkim <powerkkim@gmail.com>
2024-10-05 13:15:56 +0900

    git test vecel

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup></script>
 
 <template>
+	<h1>Vue3 test</h1>
 	<router-link to="/home">홈으로</router-link> |
 	<router-link to="/about">about</router-link> |
 	<router-link to="/member"> 회원전용 </router-link>
