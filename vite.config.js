44cce15c72771db84df9b5de3d31928f57f1312b
powerkkim <powerkkim@gmail.com>
2024-10-05 09:10:42 +0900

    server port 원복

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,14 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: '/my-app/', // 여기서 BASE_URL을 설정합니다.
+	base: '/', // 여기서 BASE_URL을 설정합니다.
+	// base: '/my-app/', // 여기서 BASE_URL을 설정합니다.
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
-	server: {
-		port: 8080,
-	},
+	// server: {
+	// 	port: 8080,
+	// },
 });
